2bebbc3e0372856d3918e8424d1d1ca6b9a4bd41
bencyrus <mahdi.mohaghegh2001@gmail.com>
2023-06-09 12:22:30 -0400

    placed customer booking context in the CustomerApp

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
-import CustomerBookingProvider from './customer/context/CustomerBookingProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
-		<CustomerBookingProvider>
-			<RouterProvider router={router} />
-		</CustomerBookingProvider>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 )
