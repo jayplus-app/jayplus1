21482c86455463ddb026ddb64c7752c070ab364b
bencyrus <mahdi.mohaghegh2001@gmail.com>
2023-06-09 12:14:32 -0400

    cleaned up the button component

--- frontend/src/shared/Button/Button.tsx
+++ frontend/src/shared/Button/Button.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Button.css'
 import { Link } from 'react-router-dom'
-import { isDisabled } from '@testing-library/user-event/dist/utils'
 
 interface ButtonProps {
 	backgroundColor?: string
