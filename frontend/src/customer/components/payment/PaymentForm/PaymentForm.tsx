028a2edf2a0d563fad93af2614e1e050fa94b452
bencyrus <mahdi.mohaghegh2001@gmail.com>
2023-07-01 13:52:48 -0400

    added stripe components

--- frontend/src/customer/components/payment/PaymentForm/PaymentForm.tsx
+++ frontend/src/customer/components/payment/PaymentForm/PaymentForm.tsx
@@ -1,5 +1,17 @@
 import Button from '../../../../shared/Button/Button'
 import './PaymentForm.css'
+import StripeCheckoutForm from './StripeCheckoutForm'
+
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import { useEffect, useState } from 'react'
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
+const stripePromise = loadStripe('pk_test_A7jK4iCYHL045qgjjfzAfPxu')
 
 interface PaymentFormProps {
 	invoiceDetails: {
@@ -19,8 +31,37 @@ interface PaymentFormProps {
 }
 
 const PaymentForm = ({ invoiceDetails }: PaymentFormProps) => {
+	const [clientSecret, setClientSecret] = useState('')
+
+	useEffect(() => {
+		// Create PaymentIntent as soon as the page loads
+		fetch('http://localhost:4242/create-payment-intent', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				billNumber: invoiceDetails.billNumber,
+				phone: '1234567890',
+			}),
+		})
+			.then((res) => {
+				return res.json()
+			})
+			.then((data) => {
+				setClientSecret(data.clientSecret)
+			})
+	}, [invoiceDetails.billNumber])
+
+	const options = {
+		clientSecret,
+	}
+
 	return (
 		<div id="payment-form">
+			{clientSecret && (
+				<Elements options={options} stripe={stripePromise}>
+					<StripeCheckoutForm />
+				</Elements>
+			)}
 			<form className="w-full mt-4">
 				<div className="flex flex-wrap">
 					<div className="w-full">
